aa37e327842b754607408416d3c8beb62f57c709
Fabio Benjamin Oliveira <guten.abend245791@gmail.com>
2026-03-18 21:29:01 -0300

    Update SystemSalesApplication.java

--- Sistema-Vendas/src/main/java/SystemSalesApplication.java
+++ Sistema-Vendas/src/main/java/SystemSalesApplication.java
@@ -1,4 +1,28 @@
-public class SystemSalesApplication {
+import javafx.application.Application;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class SystemSalesApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/telaInicial.fxml")
+        );
+
+        Scene scene = new Scene(loader.load());
+
+        stage.setTitle("Sistema de Vendas");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
